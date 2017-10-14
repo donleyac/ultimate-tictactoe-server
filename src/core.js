b490cf2a95042cb1d32b8efaf97168d8616e6342
Alex Donley <donleyac@gmail.com>
2017-10-14 10:44:42 -0400

    Restructuring JSON structure to use List instead of Map

--- src/core.js
+++ src/core.js
@@ -2,60 +2,121 @@ import {Iterable, fromJS, List, Map,is} from 'immutable';
 
 export const INITIAL_STATE = getInitial();
 export function getInitial() {
-  let board = {};
-  for(let i=0; i<3; i++) {
-    for(let j=0; j<3; j++){
-      board[""+i+j] = {
+  let board = [...Array(3).keys()].map(i => Array(3).fill(0));
+  board.forEach(row=>{
+    row.forEach((elem,index)=>{
+      row[index] = {
         board: fromJS([...Array(3).keys()].map(i => Array(3).fill(0))),
         selectable: true,
-        winner: null
+        winner: 0
       }
-    }
-  }
+    })
+  })
   return Map({
     board: fromJS(board),
-    winner: null,
+    winner: 0,
     player: 1
   });
 }
 export function placePiece(state, grid, cell, playerId) {
-  //Place Piece in cells
-  let placed = state.updateIn(
-    ["board", grid, "board"],
-    0,
-    board => board.setIn(cell, playerId)
-  );
-  //Check Winner in Grid
-  let winner = placed.updateIn(
-    ["board", grid],
-    0,
-    grid => {
-      let board = grid.get("board");
-      let new_grid;
-      //Horizontal Check
-      board.forEach(row=>{
-        if(Math.abs(row.get(0)+row.get(1)+row.get(2))===3){
-          new_grid = grid.set("winner",row.get(0));
-        }
-      });
-      //Vertical Check
-      board.forEach((row,index)=>{
-        if(Math.abs(board.get(0).get(index)+board.get(1).get(index)+board.get(2).get(index))===3){
-          new_grid=grid.set("winner",board.get(0).get(index));
-        }
-      });
-      //Diagonal Check
-      if(Math.abs(board.get(0).get(0)+board.get(1).get(1)+board.get(2).get(2))===3){
-        new_grid=grid.set("winner", board.get(0).get(0));
+  if(playerId===state.get("player") && state.get("board").get(grid[0]).get(grid[1]).get("board").get(cell[0]).get(cell[1])===0){
+    //Place Piece in cells
+    let placed = state.updateIn(
+      ["board", grid[0], grid[1], "board", cell[0], cell[1]],
+      0,
+      board => playerId
+    );
+    //Set selectable
+    let selectable = placed.updateIn(
+      ["board"],
+      0,
+      board => {
+        let new_board = [...Array(3).keys()].map(i => Array(3).fill(0));
+        board.forEach((row,r_index)=>{
+          row.forEach((elem,index)=>{
+            let grid = board.get(r_index).get(index);
+            if(r_index!==cell[0] || index!==cell[1] && board.get(cell[0]).get(cell[1]).get("winner")===0){
+              new_board[r_index][index]={board:grid.get("board"), selectable: false, winner: grid.get("winner")};
+            }
+            else {
+              new_board[r_index][index]={board:grid.get("board"), selectable: true, winner: grid.get("winner")};
+            }
+          });
+        });
+        return fromJS(new_board);
       }
-      if(Math.abs(board.get(0).get(2)+board.get(1).get(1)+board.get(2).get(0))===3){
-        new_grid=grid.set("winner", board.get(0).get(2));
+    );
+    //Check Winner in Grid
+    let grid_winner = selectable.updateIn(
+      ["board", grid[0], grid[1]],
+      0,
+      grid => {
+        let board = grid.get("board");
+        let winner = checkWinner(board);
+        return Map({board: board, selectable: grid.get("selectable"), winner: winner});
       }
-      return Map({board: board, selectable: grid.get("selectable"), winner: new_grid?new_grid.get("winner"):grid.get("winner")});
-    }
-  );
-  //Check Winner in Game
+    );
+    //Check Game Winner and change player
+    let game_winner = grid_winner.updateIn(
+      [],
+      0,
+      state => {
+        let board = state.get("board");
+        let winner = checkWinner(board);
+        return Map({board: board, winner: winner, player:playerId*-1});
+      }
+    );
+    return game_winner;
+  }
+  return {response: "Invalid Click"};
 }
+function checkWinner(board) {
+  if(Object(board.get(0).get(0))===board.get(0).get(0)) {    //Horizontal Check
+    for(let i=0; i<board.size; i++){
+      let row = board.get(i);
+      if(Math.abs(row.get(0).get("winner")+row.get(1).get("winner")+row.get(2).get("winner"))===3){
+        return row.get(0).get("winner");
+      }
+    }
+    //Vertical Check
+    for(let i=0; i<board.size; i++){
+      if(Math.abs(board.get(0).get(i).get("winner")+board.get(1).get(i).get("winner")+board.get(2).get(i).get("winner"))===3){
+        return board.get(0).get(i).get("winner");
+      }
+    }
+    //Diagonal Check
+    if(Math.abs(board.get(0).get(0).get("winner")+board.get(1).get(1).get("winner")+board.get(2).get(2).get("winner"))===3){
+      return board.get(0).get(0).get("winner");
+    }
+    if(Math.abs(board.get(0).get(2).get("winner")+board.get(1).get(1).get("winner")+board.get(2).get(0).get("winner"))===3){
+      return board.get(0).get(2).get("winner");
+    }
+  }
+  else {
+    //Horizontal Check
+    for(let i=0; i<board.size; i++){
+      let row = board.get(i);
+      if(Math.abs(row.get(0)+row.get(1)+row.get(2))===3){
+        return row.get(0);
+      }
+    }
+    //Vertical Check
+    for(let i=0; i<board.size; i++){
+      if(Math.abs(board.get(0).get(i)+board.get(1).get(i)+board.get(2).get(i))===3){
+        return board.get(0).get(i);
+      }
+    }
+    //Diagonal Check
+    if(Math.abs(board.get(0).get(0)+board.get(1).get(1)+board.get(2).get(2))===3){
+      return board.get(0).get(0);
+    }
+    if(Math.abs(board.get(0).get(2)+board.get(1).get(1)+board.get(2).get(0))===3){
+      return board.get(0).get(2);
+    }
+  }
+  return 0;
+}
+
 // export function modIndicator(state, playerId, label, value, op){
 //   if (op==="replace"){
 //     return state.updateIn(
